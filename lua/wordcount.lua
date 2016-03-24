ba152d24b6db0305b7f0a40d35812dd589dde21a
daurnimator <quae@daurnimator.com>
2016-03-24 14:29:54 +1100

    Don't use string.gmatch, it's a luajit NYI

--- lua/wordcount.lua
+++ lua/wordcount.lua
@@ -1,8 +1,30 @@
 #!/usr/bin/env lua
 
+local function nextword(str, pos)
+	-- Find non-whitespace
+	local start_word
+	for i=pos, #str do
+		local c = str:byte(i, i)
+		if c ~= 32 and c ~= 8 then
+			start_word = i
+			break
+		end
+	end
+	if start_word then
+		local end_pos = #str
+		for i=start_word+1, #str do
+			local c = str:byte(i, i)
+			if c == 32 or c == 8 then
+				end_pos = i
+				break
+			end
+		end
+		return end_pos+1, str:sub(start_word, end_pos)
+	end
+end
 local counts = setmetatable({}, {__index=function() return 0 end})
 for line in io.stdin:lines() do
-	for word in line:gmatch("%S+") do
+	for _, word in nextword, line, 1 do
 		counts[word] = counts[word] + 1
 	end
 end
